18a853f93f436ce9e31faefa34d32579aad7b55c
Bharath Kumar Reddy <bharathkumar787@icloud.com>
2025-02-15 12:57:07 +0530

    development & prod setup,p4  11:50

--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const router = express.Router()
+const ownerModel = require('../models/owner-model')
 
 router.get('/', function(req,res){
     res.send('hey from routes')
+})
+
+
+
+router.post('./create', function(req,res){
+    res.send('hey its working')
 })
 module.exports = router; 

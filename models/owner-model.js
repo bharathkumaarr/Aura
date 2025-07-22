fc5563a5f2273d27e24c0f89d24ca7d663776f25
Bharath Kumar Reddy <bharathkumar787@icloud.com>
2025-07-22 23:46:53 +0530

    owner schema added

--- models/owner-model.js
+++ models/owner-model.js
@@ -1,21 +1,21 @@
-// const mongoose = require('mongoose')
+const mongoose = require('mongoose')
 
-// const ownerSchema = mongoose.Schema({
-//     fullname: {
-//         type: String,
-//         minLength: 3,
-//         trim: true
-//     },
-//     email: String,
-//     password: String,
-//     cart:{
-//         type: Array,
-//         default:[],
-//     },
-//     isadmin: Boolean,
-//     orders: {
-//         type: Array,
-//         default: [],
-//     },
+const ownerSchema = mongoose.Schema({
+    fullname: {
+        type: String,
+        minLength: 3,
+        trim: true
+    },
+    email: String,
+    password: String,
+    cart:{
+        type: Array,
+        default:[],
+    },
+    isadmin: Boolean,
+    orders: {
+        type: Array,
+        default: [],
+    },
     
-// })
+})
